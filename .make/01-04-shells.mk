13a476973d1e57fda1a9b701f1a5d369dff7cc61
FuenSansano <fuensanosano@gmail.com>
2023-02-12 20:27:10 +0100

    fix: solving issue with DOCKER_NORMAL_USER_UUID

--- .make/01-04-shells.mk
+++ .make/01-04-shells.mk
@@ -2,15 +2,12 @@
 
 .PHONY: bash-php
 bash-php: ## Connect to PHP container.
-	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID=1000
-) $(APP_CONTAINER_NAME) /bin/bash
+	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID) $(APP_CONTAINER_NAME) /bin/bash
 
 .PHONY: bash-db
 bash-db: ## Connect to BD container.
-	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID=1000
-) $(DB_CONTAINER_NAME) /bin/bash
+	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID) $(DB_CONTAINER_NAME) /bin/bash
 
 .PHONY: bash-nginx
 bash-nginx: ## Connect to NGINX container.
-	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID=1000
-) $(NGINX_CONTAINER_NAME) /bin/bash
+	docker-compose run --rm --service-ports -u $(DOCKER_NORMAL_USER_UUID) $(NGINX_CONTAINER_NAME) /bin/bash
